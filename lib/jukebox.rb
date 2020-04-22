f7adedd4db52ec3d267debfdf8b438e5bbb07057
David Chung <dbchung3@yahoo.com>
2020-04-22 15:31:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -48,7 +48,16 @@ end
 def run 
   puts "Please enter a command:"
   user_input = gets.strip
-  if user_input == 
+  if user_input == "list"
+    list 
+    elsif user_input == "play"
+    play
+    elsif user_input == "help"
+    help 
+    elsif user_input == "exit"
+    exit_jukebox
+  else 
+    
   
 end 
   
