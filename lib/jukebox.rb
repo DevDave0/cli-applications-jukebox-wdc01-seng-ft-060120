4f13c4d5e772cc46a3a489a3057ef6e317afcc4d
David Chung <dbchung3@yahoo.com>
2020-04-22 15:44:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,6 +26,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   songs.each_with_index do |song, index|
+    case 
     if input.to_i == (index + 1) || input == song
       puts "Playing #{song}"
     end 
